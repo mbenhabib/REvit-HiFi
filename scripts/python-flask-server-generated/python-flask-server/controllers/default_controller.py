6ddf079f240695754a1e87e9003fd2ba02af7415
mike <mikejvarner@gmail.com>
2017-01-08 04:30:50 -0800

    updates to modeldata

--- scripts/python-flask-server-generated/python-flask-server/controllers/default_controller.py
+++ scripts/python-flask-server-generated/python-flask-server/controllers/default_controller.py
@@ -1,6 +1,32 @@
+from os import listdir
+from os.path import isfile, join
+from os import walk
+from pprint import pprint
 
 def modeldata_get() -> str:
-    return 'do some magic!'
+
+    watchPath = "D:\\test"
+    fileID = 0
+    modelData = {}
+
+
+
+    # modelData['modelPath'] = watchPath.split('\\')
+    dongs = watchPath
+    print(dongs)
+
+    for root, directories, filenames in walk(watchPath):
+        for filename in filenames:
+            modelData[str(fileID)] = {}
+            # if file extension is .fbx
+            modelData[str(fileID)]['modelName'] = filename
+            modelData['modelPath'] = watchPath
+            fileID += 1
+            # print(join(root, filename))
+            # # if file extension is .coordinates
+            # modelData['modelOffset'] = filename
+
+    return modelData
 
 def writemodel_post(modelDelta = None) -> str:
     return 'do some magic!'
